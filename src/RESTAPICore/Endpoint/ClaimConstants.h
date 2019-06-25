83a20dbd2f4ecb1a06cc83f7f98e342dc37a035b
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-06-25 23:52:08 +0200

    Added claim constants

---
+++ src/RESTAPICore/Endpoint/ClaimConstants.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+namespace systelab { namespace rest_api_core { namespace claim {
+
+	static const std::string SUBJECT   = "sub";
+	static const std::string ISSUED_AT = "iat";
+
+}}}
+
