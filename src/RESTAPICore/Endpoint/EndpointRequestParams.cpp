94ae6e70f3352317b153c3aef93094a5c8d4bbf3
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-06-28 21:06:16 +0200

    Added methods to loop through the whole object

--- src/RESTAPICore/Endpoint/EndpointRequestParams.cpp
+++ src/RESTAPICore/Endpoint/EndpointRequestParams.cpp
@@ -12,6 +12,29 @@ namespace systelab { namespace rest_api_core {
 	
 	EndpointRequestParams::~EndpointRequestParams() = default;
 
+
+	// String parameters
+	unsigned int EndpointRequestParams::getStringParameterCount() const
+	{
+		return (unsigned int) m_stringParameters.size();
+	}
+
+	std::vector<std::string> EndpointRequestParams::getStringParameterNames() const
+	{
+		std::vector<std::string> names;
+		for (auto parameter : m_stringParameters)
+		{
+			names.push_back(parameter.first);
+		}
+
+		return names;
+	}
+
+	bool EndpointRequestParams::hasStringParameter(const std::string& name) const
+	{
+		return (m_stringParameters.find(name) != m_stringParameters.end());
+	}
+
 	std::string EndpointRequestParams::getStringParameter(const std::string& name) const
 	{
 		auto it = m_stringParameters.find(name);
@@ -25,6 +48,34 @@ namespace systelab { namespace rest_api_core {
 		}
 	}
 
+	void EndpointRequestParams::addStringParameter(const std::string& name, const std::string& value)
+	{
+		m_stringParameters.insert({ name, value });
+	}
+
+
+	// Numeric parameters
+	unsigned int EndpointRequestParams::getNumericParameterCount() const
+	{
+		return (unsigned int) m_numericParameters.size();
+	}
+
+	std::vector<std::string> EndpointRequestParams::getNumericParameterNames() const
+	{
+		std::vector<std::string> names;
+		for (auto parameter : m_numericParameters)
+		{
+			names.push_back(parameter.first);
+		}
+
+		return names;
+	}
+
+	bool EndpointRequestParams::hasNumericParameter(const std::string& name) const
+	{
+		return (m_numericParameters.find(name) != m_numericParameters.end());
+	}
+
 	unsigned int EndpointRequestParams::getNumericParameter(const std::string& name) const
 	{
 		auto it = m_numericParameters.find(name);
@@ -36,11 +87,6 @@ namespace systelab { namespace rest_api_core {
 		{
 			throw std::runtime_error("Numeric parameter '" + name + "' not found");
 		}
-	}
-
-	void EndpointRequestParams::addStringParameter(const std::string& name, const std::string& value)
-	{
-		m_stringParameters.insert({ name, value });
 	}
 
 	void EndpointRequestParams::addNumericParameter(const std::string& name, unsigned int value)
--- src/RESTAPICore/Endpoint/EndpointRequestParams.h
+++ src/RESTAPICore/Endpoint/EndpointRequestParams.h
@@ -9,10 +9,18 @@ namespace systelab { namespace rest_api_core {
 		EndpointRequestParams();
 		virtual ~EndpointRequestParams();
 
+		// String parameters
+		unsigned int getStringParameterCount() const;
+		std::vector<std::string> getStringParameterNames() const;
+		bool hasStringParameter(const std::string& name) const;
 		std::string getStringParameter(const std::string& name) const;
-		unsigned int getNumericParameter(const std::string& name) const;
-
 		void addStringParameter(const std::string& name, const std::string& value);
+
+		// Numeric parameters
+		unsigned int getNumericParameterCount() const;
+		std::vector<std::string> getNumericParameterNames() const;
+		bool hasNumericParameter(const std::string& name) const;
+		unsigned int getNumericParameter(const std::string& name) const;
 		void addNumericParameter(const std::string& name, unsigned int value);
 
 	private:
