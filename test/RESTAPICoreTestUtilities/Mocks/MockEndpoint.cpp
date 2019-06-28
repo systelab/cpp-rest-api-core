5ebb4ae85c8e2227a2acedfa5eeee505eddebe30
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-06-28 19:47:52 +0200

    Added mock for endpoint class

---
+++ test/RESTAPICoreTestUtilities/Mocks/MockEndpoint.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "MockEndpoint.h"
+
+
+namespace systelab { namespace rest_api_core { namespace test_utility {
+
+	MockEndpoint::MockEndpoint()
+	{
+	}
+
+	MockEndpoint::~MockEndpoint() = default;
+
+}}}
+
---
+++ test/RESTAPICoreTestUtilities/Mocks/MockEndpoint.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "RESTAPICore/Endpoint/IEndpoint.h"
+
+#include "WebServerAdapterInterface/Model/Reply.h"
+
+
+namespace systelab { namespace rest_api_core { namespace test_utility {
+
+	class MockEndpoint : public IEndpoint
+	{
+	public:
+		MockEndpoint();
+		virtual ~MockEndpoint();
+
+		MOCK_METHOD1(executeProxy, web_server::Reply* (const EndpointRequestData&));
+		std::unique_ptr<web_server::Reply> execute(const EndpointRequestData& endpointRequestData)
+		{
+			return std::unique_ptr<web_server::Reply>(executeProxy(endpointRequestData));
+		}
+	};
+
+}}}
+
