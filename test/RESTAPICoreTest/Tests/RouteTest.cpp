6a729229d8e48fac9c0c515eda862272e4e57cd6
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-08-07 00:12:39 +0200

    Added missing unit tests

--- test/RESTAPICoreTest/Tests/RouteTest.cpp
+++ test/RESTAPICoreTest/Tests/RouteTest.cpp
@@ -2,9 +2,11 @@
 #include "RESTAPICore/Router/RoutesFactory.h"
 #include "RESTAPICore/Router/IRoute.h"
 
+#include "WebServerAdapterInterface/Model/Headers.h"
 #include "WebServerAdapterInterface/Model/Request.h"
 
 #include "RESTAPICoreTestUtilities/Mocks/Endpoint/MockEndpoint.h"
+#include "RESTAPICoreTestUtilities/Mocks/RouteAccess/MockRouteAccessValidator.h"
 
 #include "TestUtilitiesInterface/EntityComparator.h"
 
@@ -21,6 +23,7 @@ namespace systelab { namespace rest_api_core { namespace unit_test {
 		{
 			setUpRoutesFactory();
 			setUpEndpointFactoryMethod();
+			setUpAccessValidatorFactoryMethods();
 		}
 	
 		void setUpRoutesFactory()
@@ -54,6 +57,25 @@ namespace systelab { namespace rest_api_core { namespace unit_test {
 				};
 		}
 
+		void setUpAccessValidatorFactoryMethods()
+		{
+			m_passAccessValidatorFactoryMethod =
+				[this]() -> std::unique_ptr<IRouteAccessValidator>
+				{
+					auto routeAccessValidator = std::make_unique<MockRouteAccessValidator>();
+					ON_CALL(*routeAccessValidator, hasAccess(_)).WillByDefault(Return(true));
+					return routeAccessValidator;
+				};
+
+			m_failAccessValidatorFactoryMethod =
+				[this]() -> std::unique_ptr<IRouteAccessValidator>
+				{
+					auto routeAccessValidator = std::make_unique<MockRouteAccessValidator>();
+					ON_CALL(*routeAccessValidator, hasAccess(_)).WillByDefault(Return(false));
+					return routeAccessValidator;
+				};
+		}
+
 		std::unique_ptr<web_server::Request> buildSimpleRequest(const std::string& method,
 																const std::string& uri)
 		{
@@ -64,11 +86,30 @@ namespace systelab { namespace rest_api_core { namespace unit_test {
 			return request;
 		}
 
+		std::unique_ptr<web_server::Request> buildRequestWithHeaders(const std::string& method,
+																	 const std::string& uri,
+																	 const web_server::RequestHeaders& headers)
+		{
+			auto request = std::make_unique<web_server::Request>();
+			request->setMethod(method);
+			request->setURI(uri);
+
+			for (auto headerPair : headers.getHeadersMap())
+			{
+				request->getHeaders().addHeader(headerPair.first, headerPair.second);
+			}
+
+			return request;
+		}
+
+
 	protected:
 		std::string m_jwtKey;
 		std::unique_ptr<RoutesFactory> m_routesFactory;
 		std::function< std::unique_ptr<IEndpoint>() > m_endpointFactoryMethod;
 		MockEndpoint m_endpoint;
+		std::function< std::unique_ptr<IRouteAccessValidator>() > m_passAccessValidatorFactoryMethod;
+		std::function< std::unique_ptr<IRouteAccessValidator>() > m_failAccessValidatorFactoryMethod;
 	};
 
 
@@ -201,6 +242,204 @@ namespace systelab { namespace rest_api_core { namespace unit_test {
 
 		auto route = m_routesFactory->buildRoute("GET", "/rest/api/:param1/item/+param2/item/:param3", {}, m_endpointFactoryMethod);
 		auto request = buildSimpleRequest("GET", "/rest/api/one/item/2/item/three");
+		auto reply = route->execute(*request);
+
+		ASSERT_TRUE(reply != NULL);
+		ASSERT_EQ(web_server::Reply::OK, reply->getStatus());
+		ASSERT_EQ("{}", reply->getContent());
+	}
+
+	TEST_F(RouteTest, testRequestWithURIWithStringParameterCodification)
+	{
+		auto route = m_routesFactory->buildRoute("GET", "/rest/api/test", {}, m_endpointFactoryMethod);
+		auto request = buildSimpleRequest("GET", "/rest/api/:id");
+		auto reply = route->execute(*request);
+		ASSERT_TRUE(reply == NULL);
+	}
+
+	TEST_F(RouteTest, testRequestWithURIWithNumericParameterCodification)
+	{
+		EXPECT_CALL(m_endpoint, executeProxy(_)).Times(0);
+
+		auto route = m_routesFactory->buildRoute("GET", "/rest/api/test", {}, m_endpointFactoryMethod);
+		auto request = buildSimpleRequest("GET", "/rest/api/+id");
+		auto reply = route->execute(*request);
+		ASSERT_TRUE(reply == NULL);
+	}
+
+
+	// Headers & Authorization data
+	TEST_F(RouteTest, testRequestWithAnotherHeaderCallsEndpointWithEmptyAuthorizationData)
+	{
+		web_server::RequestHeaders requestHeaders;
+		requestHeaders.addHeader(web_server::CONTENT_TYPE, "application/json");
+
+		EndpointRequestData expectedEndpointRequestData;
+		expectedEndpointRequestData.setHeaders(requestHeaders);
+		EXPECT_CALL(m_endpoint, executeProxy(isEqualTo(expectedEndpointRequestData)));
+
+		auto route = m_routesFactory->buildRoute("GET", "/rest/api/test", {}, m_endpointFactoryMethod);
+		auto request = buildRequestWithHeaders("GET", "/rest/api/test", requestHeaders);
+		route->execute(*request);
+	}
+
+	TEST_F(RouteTest, testRequestWithAuthorizationHeaderWithoutProtocolCallsEndpointWithEmptyAuthorizationData)
+	{
+		std::string authorizationHeader = "anNtaXRoOm15cGFzc3dvcmQ=";
+		web_server::RequestHeaders requestHeaders;
+		requestHeaders.addHeader(web_server::AUTHORIZATION, authorizationHeader);
+
+		EndpointRequestData expectedEndpointRequestData;
+		expectedEndpointRequestData.setHeaders(requestHeaders);
+		EXPECT_CALL(m_endpoint, executeProxy(isEqualTo(expectedEndpointRequestData)));
+
+		auto route = m_routesFactory->buildRoute("GET", "/rest/api/test", {}, m_endpointFactoryMethod);
+		auto request = buildRequestWithHeaders("GET", "/rest/api/test", requestHeaders);
+		route->execute(*request);
+	}
+
+	TEST_F(RouteTest, testRequestWithAuthorizationHeaderForAnotherProtocolCallsEndpointWithEmptyAuthorizationData)
+	{
+		std::string authorizationHeader = "Basic anNtaXRoOm15cGFzc3dvcmQ=";
+		web_server::RequestHeaders requestHeaders;
+		requestHeaders.addHeader(web_server::AUTHORIZATION, authorizationHeader);
+
+		EndpointRequestData expectedEndpointRequestData;
+		expectedEndpointRequestData.setHeaders(requestHeaders);
+		EXPECT_CALL(m_endpoint, executeProxy(isEqualTo(expectedEndpointRequestData)));
+
+		auto route = m_routesFactory->buildRoute("GET", "/rest/api/test", {}, m_endpointFactoryMethod);
+		auto request = buildRequestWithHeaders("GET", "/rest/api/test", requestHeaders);
+		route->execute(*request);
+	}
+
+	TEST_F(RouteTest, testRequestWithAuthorizationHeaderWithBearerAndInvalidDataCallsEndpointWithEmptyAuthorizationData)
+	{
+		std::string authorizationHeader = "Bearer anNtaXRoOm15cGFzc3dvcmQ=";
+		web_server::RequestHeaders requestHeaders;
+		requestHeaders.addHeader(web_server::AUTHORIZATION, authorizationHeader);
+
+		EndpointRequestData expectedEndpointRequestData;
+		expectedEndpointRequestData.setHeaders(requestHeaders);
+		EXPECT_CALL(m_endpoint, executeProxy(isEqualTo(expectedEndpointRequestData)));
+
+		auto route = m_routesFactory->buildRoute("GET", "/rest/api/test", {}, m_endpointFactoryMethod);
+		auto request = buildRequestWithHeaders("GET", "/rest/api/test", requestHeaders);
+		route->execute(*request);
+	}
+
+	TEST_F(RouteTest, testRequestWithAuthorizationHeaderWithBearerAndValidDataCallsEndpointWithAuthorizationDataClaims)
+	{
+		std::string authorizationHeader = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
+										  "eyJzdWIiOiJqc21pdGgifQ.0-GCEvX4THSns23xDx2awrWPGPOtvJcEkQbma4nGWZg";
+		web_server::RequestHeaders requestHeaders;
+		requestHeaders.addHeader(web_server::AUTHORIZATION, authorizationHeader);
+
+		EndpointRequestAuthorizationClaims expectedAuthorizationClaims;
+		expectedAuthorizationClaims.addClaim("sub", "jsmith");
+
+		EndpointRequestData expectedEndpointRequestData;
+		expectedEndpointRequestData.setHeaders(requestHeaders);
+		expectedEndpointRequestData.setAuthorizationClaims(expectedAuthorizationClaims);
+		EXPECT_CALL(m_endpoint, executeProxy(isEqualTo(expectedEndpointRequestData)));
+
+		auto route = m_routesFactory->buildRoute("GET", "/rest/api/test", {}, m_endpointFactoryMethod);
+		auto request = buildRequestWithHeaders("GET", "/rest/api/test", requestHeaders);
+		route->execute(*request);
+	}
+
+
+	// Query strings
+	TEST_F(RouteTest, testRequestWithQueryStringsCallsEndpointWithSameQueryStrings)
+	{
+		web_server::RequestQueryStrings requestQueryStrings;
+		requestQueryStrings.addItem("sort", "field");
+		requestQueryStrings.addItem("page", "23");
+
+		EndpointRequestData expectedEndpointRequestData;
+		expectedEndpointRequestData.setQueryStrings(requestQueryStrings);
+		EXPECT_CALL(m_endpoint, executeProxy(isEqualTo(expectedEndpointRequestData)));
+
+		auto request = buildSimpleRequest("GET", "/rest/api/test");
+		for (auto queryStringItem : requestQueryStrings.getItemsMap())
+		{
+			request->getQueryStrings().addItem(queryStringItem.first, queryStringItem.second);
+		}
+
+		auto route = m_routesFactory->buildRoute("GET", "/rest/api/test", {}, m_endpointFactoryMethod);
+		route->execute(*request);
+	}
+
+
+	// Content
+	TEST_F(RouteTest, testRequestWithContentCallsEndpointWithSameContent)
+	{
+		std::string requestContent = "This is a fancy request content";
+
+		EndpointRequestData expectedEndpointRequestData;
+		expectedEndpointRequestData.setContent(requestContent);
+		EXPECT_CALL(m_endpoint, executeProxy(isEqualTo(expectedEndpointRequestData)));
+
+		auto request = buildSimpleRequest("GET", "/rest/api/test");
+		request->setContent(requestContent);
+
+		auto route = m_routesFactory->buildRoute("GET", "/rest/api/test", {}, m_endpointFactoryMethod);
+		route->execute(*request);
+	}
+
+
+	// Access validators
+	TEST_F(RouteTest, testRouteWithSingleAccessValidatorThatFailsReturnsForbiddenReply)
+	{
+		EXPECT_CALL(m_endpoint, executeProxy(_)).Times(0);
+
+		auto route = m_routesFactory->buildRoute("GET", "/rest/api/test", { m_failAccessValidatorFactoryMethod }, m_endpointFactoryMethod);
+		auto request = buildSimpleRequest("GET", "/rest/api/test");
+		auto reply = route->execute(*request);
+	
+		ASSERT_TRUE(reply != NULL);
+		ASSERT_EQ(web_server::Reply::FORBIDDEN, reply->getStatus());
+		ASSERT_EQ("{}", reply->getContent());
+	}
+
+	TEST_F(RouteTest, testRouteWithSingleAccessValidatorThatPassesCallsEndpoint)
+	{
+		EndpointRequestData expectedEndpointRequestData;
+		EXPECT_CALL(m_endpoint, executeProxy(isEqualTo(expectedEndpointRequestData)));
+
+		auto route = m_routesFactory->buildRoute("GET", "/rest/api/test", { m_passAccessValidatorFactoryMethod }, m_endpointFactoryMethod);
+		auto request = buildSimpleRequest("GET", "/rest/api/test");
+		auto reply = route->execute(*request);
+
+		ASSERT_TRUE(reply != NULL);
+		ASSERT_EQ(web_server::Reply::OK, reply->getStatus());
+		ASSERT_EQ("{}", reply->getContent());
+	}
+
+	TEST_F(RouteTest, testRouteWithMultipleAccessValidatorsAndOneFailsReturnsForbiddenReply)
+	{
+		EXPECT_CALL(m_endpoint, executeProxy(_)).Times(0);
+
+		auto route = m_routesFactory->buildRoute("GET", "/rest/api/test",
+												 { m_passAccessValidatorFactoryMethod, m_failAccessValidatorFactoryMethod,
+												   m_passAccessValidatorFactoryMethod }, m_endpointFactoryMethod);
+		auto request = buildSimpleRequest("GET", "/rest/api/test");
+		auto reply = route->execute(*request);
+
+		ASSERT_TRUE(reply != NULL);
+		ASSERT_EQ(web_server::Reply::FORBIDDEN, reply->getStatus());
+		ASSERT_EQ("{}", reply->getContent());
+	}
+
+	TEST_F(RouteTest, testRouteWithMultipleAccessValidatorsAndAllPassCallsEndpoint)
+	{
+		EndpointRequestData expectedEndpointRequestData;
+		EXPECT_CALL(m_endpoint, executeProxy(isEqualTo(expectedEndpointRequestData)));
+
+		auto route = m_routesFactory->buildRoute("GET", "/rest/api/test",
+												 { m_passAccessValidatorFactoryMethod, m_passAccessValidatorFactoryMethod,
+												   m_passAccessValidatorFactoryMethod }, m_endpointFactoryMethod);
+		auto request = buildSimpleRequest("GET", "/rest/api/test");
 		auto reply = route->execute(*request);
 
 		ASSERT_TRUE(reply != NULL);
