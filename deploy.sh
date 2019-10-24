af5a7becfbf3d8bcb53a1240f6c128e8824d1300
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-10-25 01:01:18 +0200

    Added script to deploy conan packages on travis

---
+++ deploy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+function checkErrors
+{
+	if [ $? != 0 ]
+	then
+		echo 
+		echo "An error occured while deploying artifacts."
+		echo
+		exit 1
+	fi
+}
+
+function parseParameters
+{
+	VERSION="None"
+	while getopts "hv:" OPTION
+	do
+		case $OPTION in
+		h)
+			usage #help: show usage section
+			exit 0
+			;;
+		v)
+			VERSION=$OPTARG
+			;;
+		?)
+			echo "Invalid parameter" 
+			usage
+			exit 1
+			;;
+		esac
+	done
+	
+	echo "********** DEPLOY PARAMETERS **********"
+	echo "VERSION: $VERSION"
+}
+
+parseParameters "${@}"
+
+echo -e "\nUploading RESTAPICore/$VERSION@systelab/stable to systelab-bintray...\n\n"
+conan upload "RESTAPICore/$VERSION@systelab/stable" --all -r systelab-bintray --force
+checkErrors
+
+echo -e "\nUploading RESTAPICoreTestUtilities/$VERSION@systelab/stable to systelab-bintray...\n\n"
+conan upload "RESTAPICoreTestUtilities/$VERSION@systelab/stable" --all -r systelab-bintray --force
+checkErrors
