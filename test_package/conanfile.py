e2da2bfff0fa18fca48b3bc78c58d583b4c64991
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-10-24 23:59:13 +0200

    Added gtest and OpenSSL options

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -4,6 +4,12 @@ from conans import ConanFile, CMake
 class RESTAPICoreTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake_find_package"
+    options = {"gtest": ["1.7.0", "1.8.1"], "OpenSSL": ["1.0.2n"]}
+    default_options = {"gtest":"1.8.1", "OpenSSL":"1.0.2n"}
+
+    def configure(self):
+        self.options["RESTAPICore"].gtest = self.options.gtest
+        self.options["RESTAPICore"].OpenSSL = self.options.OpenSSL
 
     def build(self):
         cmake = CMake(self)
