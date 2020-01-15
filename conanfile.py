7b74d557c6547b95566cb198f30c30a07347d64e
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-01-16 00:38:16 +0100

    Moved to version 1.1.0 of WebServerAdapter

--- conanfile.py
+++ conanfile.py
@@ -21,12 +21,12 @@ class RESTAPICoreConan(ConanFile):
         self.options["JWTUtils"].OpenSSL = self.options.OpenSSL
 
     def requirements(self):
-        self.requires("WebServerAdapterInterface/1.0.4@systelab/stable")
+        self.requires("WebServerAdapterInterface/1.1.0@systelab/stable")
         self.requires("JWTUtils/1.0.5@systelab/stable")
 
     def build_requirements(self):
         self.build_requires("TestUtilitiesInterface/1.0.3@systelab/stable")
-        self.build_requires("WebServerAdapterTestUtilities/1.0.4@systelab/stable")
+        self.build_requires("WebServerAdapterTestUtilities/1.1.0@systelab/stable")
         self.build_requires("JSONAdapterTestUtilities/1.0.8@systelab/stable")
         if self.options.gtest == "1.7.0":
             self.build_requires("gtest/1.7.0@systelab/stable")
