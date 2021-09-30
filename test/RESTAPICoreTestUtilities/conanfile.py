659511e2c1520a20f0dcce6235a649ff38dc3cfe
Joaquim Vila Bou <36737101+joaquimvila@users.noreply.github.com>
2021-09-30 18:33:18 +0200

    Added support for OpenSSL 1.1.1k

--- test/RESTAPICoreTestUtilities/conanfile.py
+++ test/RESTAPICoreTestUtilities/conanfile.py
@@ -11,8 +11,8 @@ class RESTAPICoreTestUtilitiesConan(ConanFile):
     license = "MIT"
     generators = "cmake_find_package"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"gtest": ["1.7.0", "1.8.1", "1.10.0"], "openssl": ["1.0.2n", "1.0.2s", "1.1.1g"]}
-    default_options = {"gtest":"1.10.0", "openssl":"1.1.1g"}
+    options = {"gtest": ["1.7.0", "1.8.1", "1.10.0"], "openssl": ["1.0.2n", "1.0.2s", "1.1.1g", "1.1.1k"]}
+    default_options = {"gtest":"1.10.0", "openssl":"1.1.1k"}
 
     def configure(self):
         self.options["RESTAPICore"].gtest = self.options.gtest
