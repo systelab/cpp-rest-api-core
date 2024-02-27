502c43a413ef96d91761e3f27f4936289eb89ee6
jlpeiro <jpeiro@werfen.com>
2024-02-27 09:01:36 +0100

    Changed some conan dependencies to private

--- conanfile.py
+++ conanfile.py
@@ -19,7 +19,8 @@ class RESTAPICoreConan(ConanFile):
         self.requires("WebServerAdapterInterface/2.0.2@systelab/stable")
         self.requires("JWTUtils/1.1.10@systelab/stable")
         self.requires("TimeAdapter/1.0.6@systelab/stable")
-        self.requires("gtest/1.14.0#4372c5aed2b4018ed9f9da3e218d18b3", private=True)
+
+        self.requires("gtest/1.14.0#4372c5aed2b4018ed9f9da3e218d18b3", override=True) # Hack
         self.requires("TestUtilitiesInterface/1.0.8@systelab/stable", private=True)
         self.requires("WebServerAdapterTestUtilities/2.0.2@systelab/stable", private=True)
         self.requires("JSONAdapterTestUtilities/1.1.5@systelab/stable", private=True)
